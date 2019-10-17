7ae237e175b61ab73844385ed69345c272dad86e
HoangNguyenSong <nguyensonghoang12a1@gmail.com>
2019-10-17 13:19:33 +0700

    @hoangnguyensong
    update

---
+++ DoAnCTDL/QuanLySinhVien/QuanLySinhVien/LinkedListSV.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySinhVien
+{
+    class LinkedListSV<L>
+    {
+        public LinkedListSV()
+        {
+            pHead = null;
+            pTail = null;
+            count = 0;
+        }
+
+        public class Node
+        {
+            public L data;
+            public Node pNext;
+        }
+        public Node pHead;
+        public Node pTail;
+        public int count;
+
+        public Node CreNode(L info)
+        {
+            Node pNode = new Node();
+            if (pNode != null)
+            {
+                pNode.data = info;
+                pNode.pNext = null;
+            }
+            return pNode;
+        }
+        public void AddHead(L info)
+        {
+            Node NewNode = CreNode(info);
+            if (NewNode == null)
+            {
+                return;
+            }
+            if (pHead == null)
+            {
+                pHead = pTail = NewNode;
+            }
+            else
+            {
+                NewNode.pNext = pHead;
+                pHead = NewNode;
+            }
+            count++;
+        }
+
+    }
+}
--- DoAnCTDL/QuanLySinhVien/QuanLySinhVien/SinhVien.cs
+++ DoAnCTDL/QuanLySinhVien/QuanLySinhVien/SinhVien.cs
@@ -8,10 +8,58 @@ namespace QuanLySinhVien
 {
     class SinhVien
     {
-        //demo three properties 
-        private int ID;
-        private string Name;
-        private string IDClass;
+        private int id;
+        private string name;
+        private bool sex;
+        private DateTime doB; // ngày sinh
+        private double mScores; // điểm toán
+        private double pScores; // điểm lý
+        private double cScores; // điểm hóa
 
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public bool Sex
+        {
+            get { return sex; }
+            set { sex = value; }
+        }
+
+        public DateTime DoB
+        {
+            get { return doB; }
+            set { doB = value; }
+        }
+
+        public double Mscores
+        {
+            get { return mScores; }
+            set { mScores = value; }
+        }
+
+        public double Pscores
+        {
+            get { return pScores; }
+            set { pScores = value; }
+        }
+
+        public double Cscores
+        {
+            get { return cScores; }
+            set { cScores = value; }
+        }
+
+        public double AvgScores()
+        {
+            return ((Mscores + Pscores + Cscores) / 3);
+        }
     }
 }
+
