625f0633e0352f604a0432d66f98981658fc50fc
HoangNguyenSong <nguyensonghoang12a1@gmail.com>
2019-10-17 13:10:03 +0700

    @hoangnguyensong
    update

---
+++ DoAnCTDL/QuanLySinhVien/QuanLySinhVien/SinhVien.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySinhVien
+{
+    class SinhVien
+    {
+        //demo three properties 
+        private int ID;
+        private string Name;
+        private string IDClass;
+
+    }
+}
